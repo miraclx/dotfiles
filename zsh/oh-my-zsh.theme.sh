b70275ed75ac386ff5f1e4ee854792a80c565036
Miraculous Owonubi <omiraculous@gmail.com>
2023-06-04 03:10:54 +0100

    avoid ls alias overload from omz

--- zsh/oh-my-zsh.theme.sh
+++ zsh/oh-my-zsh.theme.sh
@@ -24,7 +24,7 @@
 # DISABLE_MAGIC_FUNCTIONS="true"
 
 # Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
+DISABLE_LS_COLORS="true"
 
 # Uncomment the following line to disable auto-setting terminal title.
 # DISABLE_AUTO_TITLE="true"
